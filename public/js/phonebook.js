157041c9050b1be5fee33b890d50c22a5df9a56e
IonutBuda <ionutbuda@live.com>
2018-05-21 20:11:03 +0300

    Updated

--- public/js/phonebook.js
+++ public/js/phonebook.js
@@ -1,3 +1,17 @@
+
+function loadContacts() {
+    $.ajax({
+        url: '/phonebook',
+        method: "GET"
+    }).done(function (persons) {
+        display(persons);
+
+    });
+}
+
+
+
+
 function getRow(person) {
     return '<tr>' +
         '<td>' + person.firstName + '</td>' +
@@ -9,25 +23,43 @@ function getRow(person) {
         '</tr>'
 }
 
-var persons = [];
+function editContact(id) {
+    var editPerson = persons.find(function (person) {
+        console.log(person.firstName);
+        return person.id == id;
+    });
+    console.warn('edit', editPerson);
 
-$.ajax({
-    url: '/phonebook',
-    method: "GET"
-}).done(function (persons) {
-    display(persons);
-});
+    $('input[name=firstName]').val(editPerson.firstName);
+    $('input[name=lastName]').val(editPerson.lastName);
+    $('input[name=phone]').val(editPerson.phone);
+
+    $('.add-form').submit(function (event) {
+        event.preventDefault();
+        const person = {
+            id: id,
+            firstName: $('input[name=firstName]').val(),
+            lastName: $('input[name=lastName]').val(),
+            phone: $('input[name=phone]').val()
+
+        };
+        saveContact(person);
+
+    });
+}
+
 
 function deleteContact(id) {
     $.ajax({
         url: '/phonebook/delete',
         method: "POST",
-        data : {
+        data: {
             id: id
         }
     }).done(function (persons) {
         display(persons);
         console.warn("done", persons);
+        bindEvents(persons);
     });
 
 }
@@ -36,78 +68,58 @@ function saveContact(person) {
     $.ajax({
         url: '/phonebook/update',
         method: "POST",
-        data : person
+        data: person
     }).done(function (persons) {
         display(persons);
+        bindEvents(persons);
         console.warn("done", persons);
     });
 
 }
 
-function display(persons) {
-    var rows = '';
-
-    function createRows(person) {
-        rows += getRow(person);
-    };
-
-
-    persons.forEach(createRows);
-
-    rows += '<tr>' +
+function getActionRow() {
+    return '<tr>' +
         '<td><input type="text" name="firstName" required placeholder="Enter first name"></td>' +
         '<td><input type="text" name="lastName" placeholder="Enter last name"></td>' +
         '<td><input type="text" name="phone" required placeholder="Enter phone"></td>' +
         '<td><button type="submit" id="buton">Add</button></td>' +
         '</tr>';
+}
+
+function bindEvents(persons) {
+    $("#phone-book tbody ").delegate('a.edit',"click",function () {
+        var id = this.attributes["data-id"].value;
+        // var id = $(this).attr("data-id");
+        // var id = $(this).data('id');
+        console.info("click on", this, id);
+
+        editContact(id);
+        document.getElementById("buton").innerText = "Save";
+    });
+
+    $("#phone-book tbody ").delegate("a.delete","click",function () {
+        var id = this.attributes["data-id"].value;
+        // var id = $(this).attr("data-id");
+        // var id = $(this).data('id');
+        console.info("click on", this, id);
+
+        deleteContact(id);
+    });
+}
+
+function display(persons) {
+    window.persons = persons;
+    var rows = '';
+    function createRows(person) {
+        rows += getRow(person);
+    };
+    persons.forEach(createRows);
+    rows += getActionRow();
 
 
     $('#phone-book tbody').html(rows);
-
-    $("#phone-book tbody a.edit").click(function () {
-        var id = this.attributes["data-id"].value;
-        // var id = $(this).attr("data-id");
-        // var id = $(this).data('id');
-        console.info("click on", this, id);
-
-        editContact(id, persons);
-        document.getElementById("buton").innerText = "Save";
-    })
-
-    $("#phone-book tbody a.delete").click(function () {
-        var id = this.attributes["data-id"].value;
-        // var id = $(this).attr("data-id");
-        // var id = $(this).data('id');
-        console.info("click on", this, id);
-
-
-
-        deleteContact(id);
-    })
-    function editContact(id, persons) {
-        var editPerson = persons.find(function (person) {
-            console.log(person.firstName);
-            return person.id == id;
-        });
-        console.warn('edit', editPerson);
-
-        $('input[name=firstName]').val(editPerson.firstName);
-        $('input[name=lastName]').val(editPerson.lastName);
-        $('input[name=phone]').val(editPerson.phone);
-
-        $('.add-form').submit(function(event){
-           event.preventDefault();
-           const person = {
-            id: id,
-            firstName: $('input[name=firstName]').val(),
-            lastName: $('input[name=lastName]').val(),
-            phone: $('input[name=phone]').val()
-
-           };
-           saveContact(person);
-
-        } );
-
-    }
 }
 
+var persons = [];
+loadContacts();
+bindEvents();
